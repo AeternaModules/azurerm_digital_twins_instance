2e1ff03ab59619d6700cddef030468c2c2193595
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "digital_twins_instances" {
   description = "All digital_twins_instance resources"
   value       = azurerm_digital_twins_instance.digital_twins_instances
 }
+output "digital_twins_instances_host_name" {
+  description = "List of host_name values across all digital_twins_instances"
+  value       = [for k, v in azurerm_digital_twins_instance.digital_twins_instances : v.host_name]
+}
+output "digital_twins_instances_identity" {
+  description = "List of identity values across all digital_twins_instances"
+  value       = [for k, v in azurerm_digital_twins_instance.digital_twins_instances : v.identity]
+}
+output "digital_twins_instances_location" {
+  description = "List of location values across all digital_twins_instances"
+  value       = [for k, v in azurerm_digital_twins_instance.digital_twins_instances : v.location]
+}
+output "digital_twins_instances_name" {
+  description = "List of name values across all digital_twins_instances"
+  value       = [for k, v in azurerm_digital_twins_instance.digital_twins_instances : v.name]
+}
+output "digital_twins_instances_resource_group_name" {
+  description = "List of resource_group_name values across all digital_twins_instances"
+  value       = [for k, v in azurerm_digital_twins_instance.digital_twins_instances : v.resource_group_name]
+}
+output "digital_twins_instances_tags" {
+  description = "List of tags values across all digital_twins_instances"
+  value       = [for k, v in azurerm_digital_twins_instance.digital_twins_instances : v.tags]
+}
 
